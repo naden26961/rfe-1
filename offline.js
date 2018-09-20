6ddfe97524ffaa2afe35c69a46ddbe74d9f8181e
naden26961 <42829623+naden26961@users.noreply.github.com>
2018-09-20 15:28:04 +0700

    lp[

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1537431959,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/player-sheet2.png",
+		"images/weapon-sheet0.png",
+		"images/monster-sheet0.png",
+		"images/boom-sheet0.png",
+		"images/item1-sheet0.png",
+		"images/item2-sheet0.png",
+		"images/stone-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
